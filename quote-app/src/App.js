b62f3ee01f4e43075b392d6aedcf0e975f47075e
khaja-apaon <163125825+khaja-apaon@users.noreply.github.com>
2024-03-13 11:04:49 +0000

    commit-v3

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Shaik
+        <p> Shaik Khaja Moinuddin
           Edit <code>src/App.js</code> and save to reload.
         </p> from kurnool
         <a
