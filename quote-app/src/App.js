026624f9a891fbda2504d8006aa27f76a35aec3b
khaja-apaon <163125825+khaja-apaon@users.noreply.github.com>
2024-03-14 10:05:48 +0000

    shaik mark

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -19,6 +19,7 @@ function App() {
         <p>new task</p>
         <h1>This is a apaon</h1>
         <h1>My First Heading</h1>
+        <p> <mark> Day start with task</mark></p>
         <a
           className="App-link"
           href="https://reactjs.org"
