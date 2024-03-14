b9e095e6d01fa82d3f95875d6e3c32e08a41d22d
khaja-apaon <163125825+khaja-apaon@users.noreply.github.com>
2024-03-14 09:18:45 +0000

    shaik

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to day 4</h1>
         <h2>Interesting Day</h2>
+        <i>day task starts</i>
         <h6>Hot Afternoon</h6>
         <p> Shaik Khaja Moinuddin-V N V Pranavi- sai vara prasad-vishnu-ramakrishna-sai pranav
           Edit <code>src/App.js</code> and save to reload.
