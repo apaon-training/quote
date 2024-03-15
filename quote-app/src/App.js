97af6c7c60555936dcb641490a3c09723d7a73d2
vishnu-apaon <163125949+vishnu-apaon@users.noreply.github.com>
2024-03-15 10:50:38 +0000

    p21-changes

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -24,6 +24,7 @@ Ha! Ha! Ha!
         <h6><u>Hot Afternoon</u></h6>
         <p>html</p>
         <p>css</p>
+        <p>java</p>
         {/* <h1 style="color:Tomato;">Hello apaon members</h1> */}
         <p1>"Make it <b>work</b>, make it <b>right</b>, make it <b>fast</b>."</p1>
         <p> Shaik Khaja Moinuddin-V N V Pranavi- sai vara prasad-vishnu-ramakrishna-sai pranav-sai-apaon technologies
