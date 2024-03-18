edc1cdd461b3142700df68fd0b5137363594d076
sandeep-apaon <163397076+sandeep-apaon@users.noreply.github.com>
2024-03-18 09:53:12 +0000

    adding new heading

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -45,6 +45,7 @@ Ha! Ha! Ha!
         <p>"Empower Tomorrow, Today!"</p>
         <p> <mark> Day start with new task</mark></p>
         <button>Sales qoute 149 789</button>
+        <h>Enter the new world</h>
         <h>"Unite, Innovate, Thrive: Together, Anything is Possible!"</h>
         <b>apaon</b>
         <strong>vardhan</strong>
