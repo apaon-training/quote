f33eae1fd80e74d2b12b1fb219286d302051f7f9
vishnu-apaon <163125949+vishnu-apaon@users.noreply.github.com>
2024-03-18 10:10:18 +0000

    changes2

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to apaon</h1>
+        <p>google</p>
         <pre>
           Johny, Johny!
 Yes, Papa
