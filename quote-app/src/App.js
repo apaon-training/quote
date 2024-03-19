16fd5f683a2293119a31def5924559d5f45c5099
vishnu-apaon <163125949+vishnu-apaon@users.noreply.github.com>
2024-03-19 09:10:47 +0000

    changes

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -28,7 +28,6 @@ Ha! Ha! Ha!
         <h6><u>fun Afternoon</u></h6>
         {/* <h2 style="border: 3px solid pink;" Hello apaon members ></h2> */}
         <h2>HTML Image</h2>
-        {/* <img src="https://media.istockphoto.com/vectors/welcome-to-team-concept-vector-illustration-vector-id1217003339?k=6&m=1217003339&s=612x612&w=0&h=fc6-o1Ni4D82qim3pPuhO38_DZJjoFDPbK1_zrVqSiA=" alt="Welcome to the team" width="460" height="345"></img> */}
         <p>html</p>
         <p>css</p>
         <p>java</p>
