c3acb7ecf79431f55054b54c69f4d689d84e321d
apaon-training <163280686+apaon-training@users.noreply.github.com>
2024-03-25 10:43:47 +0000

    added comments

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -4,14 +4,21 @@ import {useEffect, useState} from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
+// 1. Learn 'useState() and useEffect() from react.dev website
+// 2. Explore 'DataTable' options from primereact.org website and see how we can get the following functionlaties
+    //.  a- Edit the row
+    //. b- Delete the row
+    //. c- Add the row
+    
 function App() {
   //Define variable to store our itemsdata
+  //const [quoteItems, setQuoteItems] = useState({records: [{fields:{'Item ID': 'testdata', 'Quantity': 92, 'Price': 1111}}]});
   const [quoteItems, setQuoteItems] = useState(null);
 
   //configure headers with postman token
   const headers = { 'Authorization': 'Bearer pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5'}
   
-  //'useEffect' used here for running fetch only once
+  //'useEffect' used here for running fetch only onceheaders
   useEffect(() => {
       //we use 'fetch' command to send http request
       fetch('https://api.airtable.com/v0/appSaehfbC49RLcln/tblJk59wskJYawprk', {headers})
@@ -32,7 +39,7 @@ function App() {
             <DataTable value={quoteItems?.records} tableStyle={{ minWidth: '50rem' }}>
               {/* Fix the item id binding */}
                 <Column field="fields.Item ID" header="Id"></Column>
-                <Column field="fields.Quantity" header="Quantity"></Column>
+                <Column field="fields.Quantity" sortable header="Quantity"></Column>
                 <Column field="fields.Price" header="Price"></Column>
             </DataTable>
       </div>
