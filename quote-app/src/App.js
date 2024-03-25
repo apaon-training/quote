04f4705e7465b7d13e5190df921a3e45c8652c44
vishnu-apaon <163125949+vishnu-apaon@users.noreply.github.com>
2024-03-25 14:39:29 +0000

    change

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -7,10 +7,14 @@ import { Column } from 'primereact/column';
 function App() {
   //Define variable to store our itemsdata
   const [quoteItems, setQuoteItems] =useState(null);
-  const headers ={ 'Authorization':'Bearer pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5}
+  const headers ={ 'Authorization':'Bearer pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5'}
   useEffect(() =>{
-    fetch(' https://api.airtable.com/v0/appSaehfbC49RLcln/tblJk59wskJYawprk')
-  })
+    fetch(' https://api.airtable.com/v0/appSaehfbC49RLcln/tblJk59wskJYawprk',{headers})
+    .then(response =>response.json())
+    .then(data =>{
+      setQuoteItems(data);
+    });
+  },[]);
 
   //configure headers with postman token - pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5
   
@@ -25,6 +29,12 @@ function App() {
       {/* Converting 'quoteItems' variable data into string for displaying purpose */}
       {/* <span>{JSON.stringify(quoteItems)}</span> */}
       <div className="card">
+      <DataTable value={quoteItems?.records} showGridlines tableStyle={{ minWidth: '50rem' }}>
+                <Column field="Item ID" header="ID"></Column>
+                <Column field="Quantity" header="Quantity"></Column>
+                <Column field="Price" header="Price"></Column>
+                
+            </DataTable>
      {/* Use primeng DataTable for showing data */}
       </div>
     </div>
