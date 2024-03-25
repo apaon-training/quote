79a320bfbbfa7e2e4da68eb81896962edc4a5f82
vishnu-apaon <163125949+vishnu-apaon@users.noreply.github.com>
2024-03-25 14:07:37 +0000

    ch

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -6,7 +6,11 @@ import { Column } from 'primereact/column';
 
 function App() {
   //Define variable to store our itemsdata
-  
+  const [quoteItems, setQuoteItems] =useState(null);
+  const headers ={ 'Authorization':'Bearer pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5}
+  useEffect(() =>{
+    fetch(' https://api.airtable.com/v0/appSaehfbC49RLcln/tblJk59wskJYawprk')
+  })
 
   //configure headers with postman token - pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5
   
