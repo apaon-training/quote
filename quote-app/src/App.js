b588b529c4ea88602fdb615fe5a20466b9708e78
Sai-apaon <163126095+Sai-apaon@users.noreply.github.com>
2024-03-26 10:45:31 +0000

    items details

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -1,18 +1,20 @@
-
 import './App.css';
 import {useEffect, useState} from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
 function App() {
-  //Define variable to store our itemsdata
-  
+  const [quoteItems, setQuoteItems] = useState(null);
+  const headers = { 'Authorization': 'Bearer pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5'}
+  useEffect(() => {
+      //we use 'fetch' command to send http request
+      fetch('https://api.airtable.com/v0/appSaehfbC49RLcln/tblJk59wskJYawprk', {headers})
+      .then( response => response.json())
+      .then(data => {
+        setQuoteItems(data);
+      });
 
-  //configure headers with postman token - pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5
-  
-  //'useEffect' used here for running fetch only once
-  // URL for get - https://api.airtable.com/v0/appSaehfbC49RLcln/tblJk59wskJYawprk
-
+  }, []);
  
 
   return (
@@ -21,7 +23,12 @@ function App() {
       {/* Converting 'quoteItems' variable data into string for displaying purpose */}
       {/* <span>{JSON.stringify(quoteItems)}</span> */}
       <div className="card">
-     {/* Use primeng DataTable for showing data */}
+            <DataTable value={quoteItems?.records} tableStyle={{ minWidth: '50rem' }}>
+              {/* Fix the item id binding */}
+                <Column field="fields.Item ID" header="Id"></Column>
+                <Column field="fields.Quantity" header="Quantity"></Column>
+                <Column field="fields.Price" header="Price"></Column>
+            </DataTable>
       </div>
     </div>
   );
