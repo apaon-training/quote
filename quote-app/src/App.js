61aac2aada8b61e174e35d7dd60a176d59f42d75
pranav-apaon <163126000+pranav-apaon@users.noreply.github.com>
2024-03-26 10:57:05 +0000

    change the

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -5,20 +5,17 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
 function App() {
-  //Define variable to store our itemsdata
-  const[qouteItems,setQuoteItems]=useState(null);
+  const [quoteItems, setQuoteItems] = useState(null);
+  const headers = { 'Authorization': 'Bearer pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5'}
+  useEffect(() => {
+      //we use 'fetch' command to send http request
+      fetch('https://api.airtable.com/v0/appSaehfbC49RLcln/tblJk59wskJYawprk', {headers})
+      .then( response => response.json())
+      .then(data => {
+        setQuoteItems(data);
+      });
 
-
-  //configure headers with postman token - pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5
-  const headers = {'Authorization': 'Bearer pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5'}
-  //'useEffect' used here for running fetch only once
-  useEffect(() =>{
-      fetch('https://api.airtable.com/v0/appSaehfbC49RLcln/tblJk59wskJYawprk',{headers}).then(response => response.json()).then(data => {setQuoteItems(data);
-    })
-  
-   },[]);
-  // URL for get - https://api.airtable.com/v0/appSaehfbC49RLcln/tblJk59wskJYawprk
-
+  }, []);
  
 
   return (
@@ -27,15 +24,15 @@ function App() {
       {/* Converting 'quoteItems' variable data into string for displaying purpose */}
       {/* <span>{JSON.stringify(quoteItems)}</span> */}
       <div className="card">
-           <DataTable value={qouteItems?.records} tableStyle={{ minWidth: '50rem' }}>
-                <Column field="fields.Item ID" header="ID"></Column>
+            <DataTable value={quoteItems?.records} tableStyle={{ minWidth: '50rem' }}>
+              {/* Fix the item id binding */}
+                <Column field="fields.Item ID" header="Id"></Column>
                 <Column field="fields.Quantity" header="Quantity"></Column>
-                <Column field="fields.Price" header="price"></Column>
-           </DataTable>
-        </div>
-    );      
+                <Column field="fields.Price" header="Price"></Column>
+            </DataTable>
       </div>
+    </div>
   );
 }
 
-export default App;
+export default App;
