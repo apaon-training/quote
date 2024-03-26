77f64d33bf2f903e0acd75106630aa1c8a77771d
khaja-apaon <163125825+khaja-apaon@users.noreply.github.com>
2024-03-26 10:44:19 +0000

    table stylling

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {useEffect, useState} from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import 'primereact/resources/themes/md-dark-indigo/theme.css';
 
 function App() {
   //Define variable to store our itemsdata
@@ -27,7 +28,7 @@ function App() {
       {/* Converting 'quoteItems' variable data into string for displaying purpose */}
       {/* <span>{JSON.stringify(quoteItems)}</span> */}
       <div className="card">
-           <DataTable value={qouteItems?.records} tableStyle={{ minWidth: '50rem' }}>
+           <DataTable value={qouteItems?.records} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '50rem' }}>
                 <Column field="fields.Item ID" header="ID"></Column>
                 <Column field="fields.Quantity" header="Quantity"></Column>
                 <Column field="fields.Price" header="price"></Column>
