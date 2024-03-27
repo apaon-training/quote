734d97d8f026adf81808d3fbc5efb94316e4f826
gkramakrishna-apaon <163125884+gkramakrishna-apaon@users.noreply.github.com>
2024-03-27 08:21:12 +0000

    write api code under the comment line

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -6,15 +6,20 @@ import { Column } from 'primereact/column';
 
 function App() {
   //Define variable to store our itemsdata
-  const[qouteItems,setQuoteItems]=useState(null);
+ 
+  const[qouteItems,setQuoteItems] = useState(null);
 
 
   //configure headers with postman token - pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5
-  const headers = {'Authorization': 'Bearer pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5'}
+  
+  const headers={'Authorization': 'bearer pat302oUGS1B2NPNR.4a95c5e60a8ac93b46d6c9c619d2e35feb2766c5a0cbb45ef562c164b4d64cc5'}
   //'useEffect' used here for running fetch only once
+  
   useEffect(() =>{
-      fetch('https://api.airtable.com/v0/appSaehfbC49RLcln/tblJk59wskJYawprk',{headers}).then(response => response.json()).then(data => {setQuoteItems(data);
-    })
+    fetch('https://api.airtable.com/v0/appSaehfbC49RLcln/tblJk59wskJYawprk',
+    {headers}).then(response => response.json())
+    .then(data => {setQuoteItems(data);
+  })
   
    },[]);
   // URL for get - https://api.airtable.com/v0/appSaehfbC49RLcln/tblJk59wskJYawprk
